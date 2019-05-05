5b0c7fd480f4c9061b2002f80fa02e92ddc0ed07
king <15029155474@163.com>
2019-05-05 21:54:45 +0800

    init commit

---
+++ src/main/java/com/sdp/Concurrecy/practiceConcurrecy/PracticeConcurrecyApplication.java
@@ -0,0 +1,13 @@
+package com.sdp.Concurrecy.practiceConcurrecy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeConcurrecyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeConcurrecyApplication.class, args);
+	}
+
+}
